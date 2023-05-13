ce78d2489eacf3abf2ddaed476cc67f07dc04e15
Pinerealm <osalami09@gmail.com>
2023-05-13 14:36:56 +0300

    Remove extra whitespace

--- models/user.py
+++ models/user.py
@@ -4,7 +4,14 @@ from .base_model import BaseModel
 
 
 class User(BaseModel):
-    """Defines the attributes of a user, inherits from BaseModel"""
+    """Defines the attributes of a user, inherits from BaseModel
+
+    Attributes:
+        email (str): The user's email address
+        password (str): The user's password
+        first_name (str): The user's first name
+        last_name (str): The user's last name
+    """
     email = ""
     password = ""
     first_name = ""
