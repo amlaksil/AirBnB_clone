121aa0d0ad035c9672ac87479db79194b3d54aae
Pinerealm <osalami09@gmail.com>
2023-05-14 11:18:11 +0300

    Import needed classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """The FileStorage module"""
 import json
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 
