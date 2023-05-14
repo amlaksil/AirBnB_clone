0672d6967b0c76fbd9e13c268404b4bf8d244bf4
amlaksil <silamlakdesye@gmail.com>
2023-05-15 00:29:45 +0300

    Avoid multiple variable declaretion in single line

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the informal string representation of the instance"""
